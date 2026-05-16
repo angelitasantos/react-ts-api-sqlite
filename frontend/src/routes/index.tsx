5cc367ef2dcb1fd22272e73614386ab7e61a36a5
Angelita Santos <angelitasantos@gmail.com>
2026-05-15 22:00:39 -0300

    feat: cria placeholders simples para rotas about e contact

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../modules/web-home/public-pages/Home';
 
+function AboutPlaceholder() {
+  return <div style={{ padding: '2rem' }}>Página Sobre em construção.</div>;
+}
+
+function ContactPlaceholder() {
+  return <div style={{ padding: '2rem' }}>Página Contato em construção.</div>;
+}
+
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutPlaceholder />} />
+        <Route path='/contact' element={<ContactPlaceholder />} />
       </Routes>
     </BrowserRouter>
   );
